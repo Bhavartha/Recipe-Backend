dc8e80ccab319cedf2d3b40c64786bc501a7025b
Bhavartha Khawale <bhavarthakhawale@gmail.com>
2020-09-30 15:56:33 +0530

    Added procfile

--- recipe/models.py
+++ recipe/models.py
@@ -1,10 +1,8 @@
-from url_shortener import db
+from recipe import db
 
-
-class Link(db.Model):
+class Name(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    short_link = db.Column(db.Text, nullable=False)
-    full_link = db.Column(db.Text, nullable=False)
+    name = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
-        return f"{self.full_link}:{self.short_link}"
+        return f"{self.name}"
--- recipe/routes.py
+++ recipe/routes.py
@@ -1,7 +1,11 @@
 from flask import render_template, url_for, flash, redirect
-
+from random import randint
 from recipe import app, db
-
+from recipe.models import Name
 
 @app.route('/')
 def home():
+    n = Name(str(randint(1,999)))
+    db.session.add(n)
+    db.session.commit()
+    return "Hello"
