5cbee9e03387b02ab530a9eae4c28baae33fad36
Bhavartha Khawale <bhavarthakhawale@gmail.com>
2020-09-30 16:07:09 +0530

    Updated DB path

--- recipe/__init__.py
+++ recipe/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
 from recipe import routes
