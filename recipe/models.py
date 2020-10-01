40b318a7dc55577a96bb8704d13eb75987524ce2
Bhavartha Khawale <bhavarthakhawale@gmail.com>
2020-10-01 22:49:37 +0530

    Created Ingredients and Steps Model

--- recipe/models.py
+++ recipe/models.py
@@ -14,9 +14,23 @@ class User(db.Model):
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    upvotes = db.Column(db.Integer)
-    downvotes = db.Column(db.Integer)
+    upvotes = db.Column(db.Integer,default=False)
+    downvotes = db.Column(db.Integer,default=0)
     author_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    recipe_steps = db.relationship('RecipeSteps',backref='recipe')
+    recipe_ingredients = db.relationship('RecipeIngredients',backref='recipe')
 
     def __repr__(self):
         return f"{self.name}"
+
+class RecipeSteps(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    step_no = db.Column(db.Integer, nullable=False)
+    info = db.Column(db.Text,nullable=False)
+    recipe_id = db.Column(db.Integer,db.ForeignKey('recipe.id'))
+
+class RecipeIngredients(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.String(20))
+    name = db.Column(db.Text,nullable=False)
+    recipe_id = db.Column(db.Integer,db.ForeignKey('recipe.id'))
